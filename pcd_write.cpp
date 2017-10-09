f8972ed24010d8939d9320dbb58181c0b3102945
Nattaon <okanplusz@gmail.com>
2017-10-09 15:09:12 +0900

    different 6 face

--- pcd_write.cpp
+++ pcd_write.cpp
@@ -12,7 +12,8 @@ main (int argc, char** argv)
 	pcl::TextureMesh mesh7;
 	//pcl::io::loadPolygonFileOBJ("BlueSaltCube_5k_tex.obj", mesh7); // no texture
 
-	pcl::io::loadOBJFile("../obj2/cubfox4-e.obj", mesh7); 
+	//pcl::io::loadOBJFile("../obj2/cubfox4-e.obj", mesh7); 
+	pcl::io::loadOBJFile("../obj2/rectanglebox.obj", mesh7);
 
 	pcl::visualization::PCLVisualizer viewer_pre7("PCL Viewer_pre7");
 	viewer_pre7.addTextureMesh(mesh7, "texture", 0);
